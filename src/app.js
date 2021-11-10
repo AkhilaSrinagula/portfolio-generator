52289c7559804db93f5e29c625a1efd271a87c8a
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-11-10 01:57:55 -0600

    page-template

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+
 
 const promptUser = () => {
 return inquirer.prompt([
@@ -126,18 +129,16 @@ return inquirer.prompt([
       }
     });
 };
-  promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  const pageHTML = generatePage(portfolioData);
+
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
   });
+});
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
